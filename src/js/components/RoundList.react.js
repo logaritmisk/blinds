321b21772e96e1fe7dbef15088fbb073d959e835
logaritmisk <anders.e.olsson@gmail.com>
2015-10-08 15:03:01 +0200

    It kinda works now.

--- src/js/components/RoundList.react.js
+++ src/js/components/RoundList.react.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import classNames from 'classnames'
+
 import RoundActionCreators from '../actions/RoundActionCreators'
 
 import RoundStore from '../stores/RoundStore'
@@ -26,8 +28,14 @@ class RoundList extends Component {
     return (
       <div id="rounds">
         {this.state.rounds.map((round, i) => {
+          let classes = ['round-item']
+
+          if (i == this.state.active) {
+            classes.push('active')
+          }
+
           return (
-            <div key={i} className="round-item" onClick={event => { RoundActionCreators.setActiveRound(i) }}>
+            <div key={i} className={classNames(classes)} onClick={event => { RoundActionCreators.setActiveRound(i) }}>
               <span className="round-number">{i + 1}</span>
               <RoundBlinds
                 smallBlind={round.smallBlind}
